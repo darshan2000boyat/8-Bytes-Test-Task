ea6db3b72ba5140c7a08bff05373fd15f6d8ddfc
Darshan Boyat <forstudypurpose129@gmail.com>
2025-09-08 20:29:02 +0530

    Apply Metadata to each page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,37 @@
 import React from "react";
 import Sidebar from "./components/Sidebar";
 import DashboardLayout from "./components/DashboardLayout";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "My Portfolio Dashboard | Personal Finance Tracker",
+  description:
+    "Track your investments, stocks, and financial growth in one place. Get live data from Google Finance and Yahoo Finance.",
+  keywords: [
+    "portfolio tracker",
+    "stock dashboard",
+    "personal finance",
+    "investment portfolio",
+    "financial dashboard",
+  ],
+  openGraph: {
+    title: "My Portfolio Dashboard",
+    description:
+      "A personal finance dashboard to monitor your investments and stock performance.",
+    url: "https://yourdomain.com",
+    siteName: "My Portfolio Dashboard",
+    images: [
+      {
+        url: "https://8-bytes-test-task.vercel.app/og-image-dashboard.png",
+        width: 1200,
+        height: 630,
+        alt: "Portfolio Dashboard Preview",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+};
 
 export default function Home() {
   return (
--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,5 +1,36 @@
 import React from "react";
 import PortfolioTable from "../components/PortfolioTable";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "My Portfolio | Stocks & Investments",
+  description:
+    "Explore your stock holdings, gain/loss performance, and valuation breakdowns powered by Yahoo Finance and Google Finance APIs.",
+  keywords: [
+    "portfolio",
+    "stock analysis",
+    "investment tracker",
+    "equity portfolio",
+    "financial insights",
+  ],
+  openGraph: {
+    title: "My Investment Portfolio",
+    description:
+      "Detailed portfolio view with live stock updates, valuations, and performance charts.",
+    url: "https://8-bytes-test-task.vercel.app/portfolio",
+    siteName: "My Portfolio Dashboard",
+    images: [
+      {
+        url: "https://8-bytes-test-task.vercel.app/og-image-portfolio.png",
+        width: 1200,
+        height: 630,
+        alt: "Portfolio Page Preview",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+};
 
 const Portfolio = () => {
   return <PortfolioTable />;
